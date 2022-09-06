c116408986e2f953d44328e94de5229cbc103387
nshcore <nshcore@protonmail.com>
2022-09-06 09:05:39 -0700

    feat: add beach biome type

--- main.js
+++ main.js
@@ -416,11 +416,11 @@ function applyDefaultBiomesSystem() {
     "Glacier",
     "Swamp",
     "Badlands",
-    "Beach",
     "Stone",
     "The Void",
     "Moon Touched",
-    "Forbidden Forests"
+    "Forbidden Forests",
+    "Beach"
   ];
   const color = [
     "#466eab",
@@ -431,12 +431,12 @@ function applyDefaultBiomesSystem() {
     "#E7F9F0",
     "#ffffff",
     "#18200A",
-    "#743D20",
     "#e7f9f0",
     "#5c6360",
     "#171818",
     "#F945C0",
-    "#05190a"
+    "#05190a",
+    "#743D20"
   ];
 
   const habitability = [0, 4, 10, 22, 30, 50, 100, 80, 90, 12, 4, 0, 12, 0];
@@ -700,7 +700,7 @@ async function generate(options) {
     Rivers.specify();
     Lakes.generateName();
 
-    Military.generate();
+    // Military.generate();
     Markers.generate();
     addZones();
 
@@ -1382,6 +1382,7 @@ function reMarkFeatures() {
     features = (pack.features = [0]);
   cells.f = new Uint16Array(cells.i.length); // cell feature number
   cells.t = new Int8Array(cells.i.length); // cell type: 1 = land along coast; -1 = water along coast;
+  console.log(cells.t);// if 1 it is a biome next to water so should be beach. -1 is marine greater than 1 they are land locked
   cells.haven = cells.i.length < 65535 ? new Uint16Array(cells.i.length) : new Uint32Array(cells.i.length); // cell haven (opposite water cell);
   cells.harbor = new Uint8Array(cells.i.length); // cell harbor (number of adjacent water cells);
 
@@ -1456,32 +1457,19 @@ function isWetLand(moisture, temperature, height) {
   return false;
 }
 
+function isBeach(moisture, temperature, height, cell) {
+  if (cell === 1) return true; //near coast
+
+  return false;
+}
+
 // assign biome id for each cell
 function defineBiomes() {
   TIME && console.time("defineBiomes");
   const {cells} = pack;
+  console.log(cells)
   const {temp, prec} = grid.cells;
   cells.biome = new Uint8Array(cells.i.length); // biomes array
-
-  /**
-   * 
-  const name = [
-    "Marine",
-    "Plains",
-    "Forest",
-    "Desert",
-    "Oasis",
-    "Snow",
-    "Glacier",
-    "Swamp",
-    "Badlands",
-    "Beach",
-    "Stone",
-    "The Void",
-    "Moon Touched",
-    "Forbidden Forests"
-  ];
-   */
 
   const biomePercentages = [
     -1, //"Marine",0
@@ -1493,11 +1481,10 @@ function defineBiomes() {
     2,//"Glacier",6
     8,//"Swamp",7
     5, //"Badlands",8
-    12, //"Beach",9
-    5,//"Stone",10
-    2,//"The Void",11
-    1,//"Moon Touched",12
-    1,//"Forbidden Forests",13
+    5,//"Stone",9
+    2,//"The Void",10
+    1,//"Moon Touched",11
+    1,//"Forbidden Forests",12
   ];
   let oceanBiome = 0;
   let cellCount = 0;
@@ -1506,14 +1493,16 @@ function defineBiomes() {
     const height = cells.h[i];
     const moisture = height < 20 ? 0 : calculateMoisture(i);
     const isOcean = height < 20;
-   
+
     if(isOcean) {
+      console.log("isOcean")
       oceanBiome += 1;
     } else {
       cellCount += 1;
     }
   }
-  
+  console.log(biomePercentages.length)
+
   let biomeCount = [];
   for(let i = 0; i < biomePercentages.length; i++) {
     biomeCount.push(0);
@@ -1522,6 +1511,8 @@ function defineBiomes() {
     biomeCount[i] = Math.round((biomePercentages[i] / 100) * cellCount);
   }
 
+  console.log(biomeCount)
+
   for (const i of cells.i) {
     const temperature = temp[cells.g[i]];
     const height = cells.h[i];
@@ -1529,24 +1520,27 @@ function defineBiomes() {
     const isOcean = height < 20;
     let biomeId = -1;
 
+
     if(isOcean) {
       biomeId = 0;
     } else if (temperature < -5 && biomeCount[6] > 0) {
       biomeId = 6; // permafrost glacier biome
-    }  else if (isWetLand(moisture, temperature, height) && biomeCount[9] > 0) { 
-      biomeId = 9; // beach biome
+    }  else if (isBeach(moisture, temperature, height, cells.t[i])) {
+      biomeId = 13; // beach biome
     }
 
-    if(biomeId === -1) {      
+    if(biomeId === -1) {
       let availableBiomes = []
       for(let i = 1; i < biomeCount.length; i++) {
           if(biomeCount[i] > 0) {
+            console.log(123)
             availableBiomes.push(i);
           }
       }
 
-      if(availableBiomes.length == 0) {
-        for(let i = 1; i < biomeCount.length; i++) {            
+      if(availableBiomes.length === 0) {
+        for(let i = 1; i < biomeCount.length; i++) {
+          console.log(345)
           availableBiomes.push(i);
         }
       }
@@ -1569,17 +1563,6 @@ function defineBiomes() {
   }
 
   TIME && console.timeEnd("defineBiomes");
-}
-
-// assign biome id to a cell
-function getBiomeId(moisture, temperature, height) {
-  if (height < 20) return 0; // marine biome: all water cells
-  if (temperature < -5) return 6; // permafrost glacier biome
-  if (isWetLand(moisture, temperature, height)) return 9; // beach biome
-
-  const moistureBand = Math.min((moisture / 5) | 0, 4); // [0-4]
-  const temperatureBand = Math.min(Math.max(20 - temperature, 0), 25); // [0-25]
-  return biomesData.biomesMartix[moistureBand][temperatureBand];
 }
 
 // assess cells suitability to calculate population and rand cells for culture center and burgs placement
@@ -1631,344 +1614,7 @@ function addZones(number = 1) {
   const used = new Uint8Array(cells.i.length); // to store used cells
   const zonesData = [];
 
-  for (let i = 0; i < rn(Math.random() * 1.8 * number); i++) addInvasion(); // invasion of enemy lands
-  for (let i = 0; i < rn(Math.random() * 1.6 * number); i++) addRebels(); // rebels along a state border
-  for (let i = 0; i < rn(Math.random() * 1.6 * number); i++) addProselytism(); // proselitism of organized religion
-  for (let i = 0; i < rn(Math.random() * 1.6 * number); i++) addCrusade(); // crusade on heresy lands
-  for (let i = 0; i < rn(Math.random() * 1.8 * number); i++) addDisease(); // disease starting in a random city
-  for (let i = 0; i < rn(Math.random() * 1.4 * number); i++) addDisaster(); // disaster starting in a random city
-  for (let i = 0; i < rn(Math.random() * 1.4 * number); i++) addEruption(); // volcanic eruption aroung volcano
-  for (let i = 0; i < rn(Math.random() * 1.0 * number); i++) addAvalanche(); // avalanche impacting highland road
-  for (let i = 0; i < rn(Math.random() * 1.4 * number); i++) addFault(); // fault line in elevated areas
-  for (let i = 0; i < rn(Math.random() * 1.4 * number); i++) addFlood(); // flood on river banks
-  for (let i = 0; i < rn(Math.random() * 1.2 * number); i++) addTsunami(); // tsunami starting near coast
-
   drawZones();
-
-  function addInvasion() {
-    const atWar = states.filter(s => s.diplomacy && s.diplomacy.some(d => d === "Enemy"));
-    if (!atWar.length) return;
-
-    const invader = ra(atWar);
-    const target = invader.diplomacy.findIndex(d => d === "Enemy");
-
-    const cell = ra(cells.i.filter(i => cells.state[i] === target && cells.c[i].some(c => cells.state[c] === invader.i)));
-    if (!cell) return;
-
-    const cellsArray = [],
-      queue = [cell],
-      power = rand(5, 30);
-
-    while (queue.length) {
-      const q = P(0.4) ? queue.shift() : queue.pop();
-      cellsArray.push(q);
-      if (cellsArray.length > power) break;
-
-      cells.c[q].forEach(e => {
-        if (used[e]) return;
-        if (cells.state[e] !== target) return;
-        used[e] = 1;
-        queue.push(e);
-      });
-    }
-
-    const invasion = rw({
-      Invasion: 4,
-      Occupation: 3,
-      Raid: 2,
-      Conquest: 2,
-      Subjugation: 1,
-      Foray: 1,
-      Skirmishes: 1,
-      Incursion: 2,
-      Pillaging: 1,
-      Intervention: 1
-    });
-    const name = getAdjective(invader.name) + " " + invasion;
-    zonesData.push({name, type: "Invasion", cells: cellsArray, fill: "url(#hatch1)"});
-  }
-
-  function addRebels() {
-    const state = ra(states.filter(s => s.i && !s.removed && s.neighbors.some(n => n)));
-    if (!state) return;
-
-    const neib = ra(state.neighbors.filter(n => n && !states[n].removed));
-    if (!neib) return;
-    const cell = cells.i.find(i => cells.state[i] === state.i && !state.removed && cells.c[i].some(c => cells.state[c] === neib));
-    const cellsArray = [];
-    const queue = [];
-    if (cell) queue.push(cell);
-
-    const power = rand(10, 30);
-
-    while (queue.length) {
-      const q = queue.shift();
-      cellsArray.push(q);
-      if (cellsArray.length > power) break;
-
-      cells.c[q].forEach(e => {
-        if (used[e]) return;
-        if (cells.state[e] !== state.i) return;
-        used[e] = 1;
-        if (e % 4 !== 0 && !cells.c[e].some(c => cells.state[c] === neib)) return;
-        queue.push(e);
-      });
-    }
-
-    const rebels = rw({Rebels: 5, Insurgents: 2, Mutineers: 1, Rioters: 1, Separatists: 1, Secessionists: 1, Insurrection: 2, Rebellion: 1, Conspiracy: 2});
-    const name = getAdjective(states[neib].name) + " " + rebels;
-    zonesData.push({name, type: "Rebels", cells: cellsArray, fill: "url(#hatch3)"});
-  }
-
-  function addProselytism() {
-    const organized = ra(pack.religions.filter(r => r.type === "Organized"));
-    if (!organized) return;
-
-    const cell = ra(cells.i.filter(i => cells.religion[i] && cells.religion[i] !== organized.i && cells.c[i].some(c => cells.religion[c] === organized.i)));
-    if (!cell) return;
-    const target = cells.religion[cell];
-    const cellsArray = [],
-      queue = [cell],
-      power = rand(10, 30);
-
-    while (queue.length) {
-      const q = queue.shift();
-      cellsArray.push(q);
-      if (cellsArray.length > power) break;
-
-      cells.c[q].forEach(e => {
-        if (used[e]) return;
-        if (cells.religion[e] !== target) return;
-        if (cells.h[e] < 20) return;
-        used[e] = 1;
-        //if (e%2 !== 0 && !cells.c[e].some(c => cells.state[c] === neib)) return;
-        queue.push(e);
-      });
-    }
-
-    const name = getAdjective(organized.name.split(" ")[0]) + " Proselytism";
-    zonesData.push({name, type: "Proselytism", cells: cellsArray, fill: "url(#hatch6)"});
-  }
-
-  function addCrusade() {
-    const heresy = ra(pack.religions.filter(r => r.type === "Heresy"));
-    if (!heresy) return;
-
-    const cellsArray = cells.i.filter(i => !used[i] && cells.religion[i] === heresy.i);
-    if (!cellsArray.length) return;
-    cellsArray.forEach(i => (used[i] = 1));
-
-    const name = getAdjective(heresy.name.split(" ")[0]) + " Crusade";
-    zonesData.push({name, type: "Crusade", cells: cellsArray, fill: "url(#hatch6)"});
-  }
-
-  function addDisease() {
-    const burg = ra(burgs.filter(b => !used[b.cell] && b.i && !b.removed)); // random burg
-    if (!burg) return;
-
-    const cellsArray = [],
-      cost = [],
-      power = rand(20, 37);
-    const queue = new PriorityQueue({comparator: (a, b) => a.p - b.p});
-    queue.queue({e: burg.cell, p: 0});
-
-    while (queue.length) {
-      const next = queue.dequeue();
-      if (cells.burg[next.e] || cells.pop[next.e]) cellsArray.push(next.e);
-      used[next.e] = 1;
-
-      cells.c[next.e].forEach(function (e) {
-        const r = cells.road[next.e];
-        const c = r ? Math.max(10 - r, 1) : 100;
-        const p = next.p + c;
-        if (p > power) return;
-
-        if (!cost[e] || p < cost[e]) {
-          cost[e] = p;
-          queue.queue({e, p});
-        }
-      });
-    }
-
-    const adjective = () => ra(["Great", "Silent", "Severe", "Blind", "Unknown", "Loud", "Deadly", "Burning", "Bloody", "Brutal", "Fatal"]);
-    const animal = () => ra(["Ape", "Bear", "Boar", "Cat", "Cow", "Dog", "Pig", "Fox", "Bird", "Horse", "Rat", "Raven", "Sheep", "Spider", "Wolf"]);
-    const color = () => ra(["Golden", "White", "Black", "Red", "Pink", "Purple", "Blue", "Green", "Yellow", "Amber", "Orange", "Brown", "Grey"]);
-
-    const type = rw({Fever: 5, Pestilence: 2, Flu: 2, Pox: 2, Smallpox: 2, Plague: 4, Cholera: 2, Dropsy: 1, Leprosy: 2});
-    const name = rw({[color()]: 4, [animal()]: 2, [adjective()]: 1}) + " " + type;
-    zonesData.push({name, type: "Disease", cells: cellsArray, fill: "url(#hatch12)"});
-  }
-
-  function addDisaster() {
-    const burg = ra(burgs.filter(b => !used[b.cell] && b.i && !b.removed)); // random burg
-    if (!burg) return;
-
-    const cellsArray = [],
-      cost = [],
-      power = rand(5, 25);
-    const queue = new PriorityQueue({comparator: (a, b) => a.p - b.p});
-    queue.queue({e: burg.cell, p: 0});
-
-    while (queue.length) {
-      const next = queue.dequeue();
-      if (cells.burg[next.e] || cells.pop[next.e]) cellsArray.push(next.e);
-      used[next.e] = 1;
-
-      cells.c[next.e].forEach(function (e) {
-        const c = rand(1, 10);
-        const p = next.p + c;
-        if (p > power) return;
-
-        if (!cost[e] || p < cost[e]) {
-          cost[e] = p;
-          queue.queue({e, p});
-        }
-      });
-    }
-
-    const type = rw({Famine: 5, Dearth: 1, Drought: 3, Earthquake: 3, Tornadoes: 1, Wildfires: 1});
-    const name = getAdjective(burg.name) + " " + type;
-    zonesData.push({name, type: "Disaster", cells: cellsArray, fill: "url(#hatch5)"});
-  }
-
-  function addEruption() {
-    const volcano = document.getElementById("markers").querySelector("use[data-id='#marker_volcano']");
-    if (!volcano) return;
-
-    const x = +volcano.dataset.x,
-      y = +volcano.dataset.y,
-      cell = findCell(x, y);
-    const id = volcano.id;
-    const note = notes.filter(n => n.id === id);
-
-    if (note[0]) note[0].legend = note[0].legend.replace("Active volcano", "Erupting volcano");
-    const name = note[0] ? note[0].name.replace(" Volcano", "") + " Eruption" : "Volcano Eruption";
-
-    const cellsArray = [],
-      queue = [cell],
-      power = rand(10, 30);
-
-    while (queue.length) {
-      const q = P(0.5) ? queue.shift() : queue.pop();
-      cellsArray.push(q);
-      if (cellsArray.length > power) break;
-      cells.c[q].forEach(e => {
-        if (used[e] || cells.h[e] < 20) return;
-        used[e] = 1;
-        queue.push(e);
-      });
-    }
-
-    zonesData.push({name, type: "Disaster", cells: cellsArray, fill: "url(#hatch7)"});
-  }
-
-  function addAvalanche() {
-    const roads = cells.i.filter(i => !used[i] && cells.road[i] && cells.h[i] >= 70);
-    if (!roads.length) return;
-
-    const cell = +ra(roads);
-    const cellsArray = [],
-      queue = [cell],
-      power = rand(3, 15);
-
-    while (queue.length) {
-      const q = P(0.3) ? queue.shift() : queue.pop();
-      cellsArray.push(q);
-      if (cellsArray.length > power) break;
-      cells.c[q].forEach(e => {
-        if (used[e] || cells.h[e] < 65) return;
-        used[e] = 1;
-        queue.push(e);
-      });
-    }
-
-    const proper = getAdjective(Names.getCultureShort(cells.culture[cell]));
-    const name = proper + " Avalanche";
-    zonesData.push({name, type: "Disaster", cells: cellsArray, fill: "url(#hatch5)"});
-  }
-
-  function addFault() {
-    const elevated = cells.i.filter(i => !used[i] && cells.h[i] > 50 && cells.h[i] < 70);
-    if (!elevated.length) return;
-
-    const cell = ra(elevated);
-    const cellsArray = [],
-      queue = [cell],
-      power = rand(3, 15);
-
-    while (queue.length) {
-      const q = queue.pop();
-      if (cells.h[q] >= 20) cellsArray.push(q);
-      if (cellsArray.length > power) break;
-      cells.c[q].forEach(e => {
-        if (used[e] || cells.r[e]) return;
-        used[e] = 1;
-        queue.push(e);
-      });
-    }
-
-    const proper = getAdjective(Names.getCultureShort(cells.culture[cell]));
-    const name = proper + " Fault";
-    zonesData.push({name, type: "Disaster", cells: cellsArray, fill: "url(#hatch2)"});
-  }
-
-  function addFlood() {
-    const fl = cells.fl.filter(fl => fl),
-      meanFlux = d3.mean(fl),
-      maxFlux = d3.max(fl),
-      flux = (maxFlux - meanFlux) / 2 + meanFlux;
-    const rivers = cells.i.filter(i => !used[i] && cells.h[i] < 50 && cells.r[i] && cells.fl[i] > flux && cells.burg[i]);
-    if (!rivers.length) return;
-
-    const cell = +ra(rivers),
-      river = cells.r[cell];
-    const cellsArray = [],
-      queue = [cell],
-      power = rand(5, 30);
-
-    while (queue.length) {
-      const q = queue.pop();
-      cellsArray.push(q);
-      if (cellsArray.length > power) break;
-
-      cells.c[q].forEach(e => {
-        if (used[e] || cells.h[e] < 20 || cells.r[e] !== river || cells.h[e] > 50 || cells.fl[e] < meanFlux) return;
-        used[e] = 1;
-        queue.push(e);
-      });
-    }
-
-    const name = getAdjective(burgs[cells.burg[cell]].name) + " Flood";
-    zonesData.push({name, type: "Disaster", cells: cellsArray, fill: "url(#hatch13)"});
-  }
-
-  function addTsunami() {
-    const coastal = cells.i.filter(i => !used[i] && cells.t[i] === -1 && pack.features[cells.f[i]].type !== "lake");
-    if (!coastal.length) return;
-
-    const cell = +ra(coastal);
-    const cellsArray = [],
-      queue = [cell],
-      power = rand(10, 30);
-
-    while (queue.length) {
-      const q = queue.shift();
-      if (cells.t[q] === 1) cellsArray.push(q);
-      if (cellsArray.length > power) break;
-
-      cells.c[q].forEach(e => {
-        if (used[e]) return;
-        if (cells.t[e] > 2) return;
-        if (pack.features[cells.f[e]].type === "lake") return;
-        used[e] = 1;
-        queue.push(e);
-      });
-    }
-
-    const proper = getAdjective(Names.getCultureShort(cells.culture[cell]));
-    const name = proper + " Tsunami";
-    zonesData.push({name, type: "Disaster", cells: cellsArray, fill: "url(#hatch13)"});
-  }
 
   function drawZones() {
     zones
